15da5b68d3930c869b27343c851bcc0702d20ef8
Oleksandr Rad <clavigo365@gmail.com>
2024-10-16 16:43:05 +0300

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,28 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add: (num) => calculator.result + num,
+    subtract: (num) => calculator.result - num,
+    multiply: (num) => calculator.result * num,
+    divide: (num) =>
+      num !== 0 ? calculator.result / num : 'Error: Division by zero',
+
+    operate(callback, num) {
+      calculator.result = callback(num);
+
+      return this;
+    },
+
+    reset() {
+      calculator.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
